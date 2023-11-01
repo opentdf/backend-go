b531025a924c23ee2f31e2e4f28a64e15c157459
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2023-11-01 13:39:42 +0200

    Add TestAttrDissemFailure3 test case

---
+++ pkg/access/fetchAttributes_test.go
@@ -0,0 +1,23 @@
+package access
+
+import (
+	"testing"
+)
+
+func TestFetchAttributesSuccess(t *testing.T) {
+	//var nameSpaces = []string{"one", "two"}
+	//res := testutil.CreateRequestContext("GET", "/api/entry/get", "", controllers.GetAllEntry)
+	//
+	//attrDefs, err := fetchAttributesForNamespace(ctx, "nameSpaces")
+	//if err != nil {
+	//	t.Error(err)
+	//}
+	//
+	//if len(attrDefs) > 0 {
+	//	t.Errorf("Output %v not equal to expected %v", attrDefs, "expected")
+	//}
+}
+
+func TestFetchAttributesFailure(t *testing.T) {
+
+}
