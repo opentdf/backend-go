69c914199bafa674d11ff9cf26b2c9a4b1cd54a2
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2023-11-01 16:48:36 +0200

    Add TestFetchAttributes

--- pkg/access/fetchAttributes_test.go
+++ pkg/access/fetchAttributes_test.go
@@ -1,23 +1,53 @@
 package access
 
 import (
+	"context"
+	"github.com/jarcoal/httpmock"
+	"github.com/virtru/access-pdp/attributes"
+	"net/http"
 	"testing"
 )
 
-func TestFetchAttributesSuccess(t *testing.T) {
-	//var nameSpaces = []string{"one", "two"}
-	//res := testutil.CreateRequestContext("GET", "/api/entry/get", "", controllers.GetAllEntry)
-	//
-	//attrDefs, err := fetchAttributesForNamespace(ctx, "nameSpaces")
-	//if err != nil {
-	//	t.Error(err)
-	//}
-	//
-	//if len(attrDefs) > 0 {
-	//	t.Errorf("Output %v not equal to expected %v", attrDefs, "expected")
-	//}
-}
+func TestFetchAttributes(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
 
-func TestFetchAttributesFailure(t *testing.T) {
+	ctx := context.Background()
+	namespaces := []string{"namespace1", "namespace2"}
 
+	mockDefinitions := []attributes.AttributeDefinition{
+		{
+			Authority: "namespace1",
+			Name:      "attribute1",
+			Rule:      "rule1",
+			State:     "active",
+			Order:     []string{"value1", "value2", "value3"},
+			//GroupBy: &AttributeInstance{
+			//	// Populate the fields of AttributeInstance as needed
+			//},
+		},
+		{
+			Authority: "namespace2",
+			Name:      "attribute2",
+			Rule:      "rule2",
+			Order:     []string{"valueA", "valueB", "valueC"},
+		},
+	}
+
+	httpmock.RegisterResponder("GET", "http://localhost:65432/api/attributes/v1/attrName",
+		func(req *http.Request) (*http.Response, error) {
+			resp, err := httpmock.NewJsonResponse(200, mockDefinitions)
+			return resp, err
+		},
+	)
+
+	output, err := fetchAttributes(ctx, namespaces)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(output) == 0 {
+		t.Errorf("Output %v not equal to expected %v", output, mockDefinitions)
+	}
 }
---
+++ pkg/access/publicKey_test.go
@@ -0,0 +1,17 @@
+package access
+
+import (
+	"testing"
+)
+
+//http.HandleFunc("/kas_public_key", kas.CertificateHandler)
+//http.HandleFunc("/v2/kas_public_key", kas.PublicKeyHandlerV2)
+//http.HandleFunc("/v2/rewrap", kas.Handler)
+
+func TestCertificateSuccess(t *testing.T) {
+
+}
+
+func TestPublicKeyHandlerV2(t *testing.T) {
+
+}
