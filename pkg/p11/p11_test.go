4bc874de35dbf3b4af844d81a1ff62607b322584
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2023-11-03 16:50:07 +0200

    Additional tests for NewSession, NewPrivateKeyRSA

--- pkg/p11/p11_test.go
+++ pkg/p11/p11_test.go
@@ -3,8 +3,34 @@ package p11
 import (
 	"crypto"
 	"github.com/miekg/pkcs11"
+	"reflect"
 	"testing"
 )
+
+func TestNewSession(t *testing.T) {
+	ctx := &pkcs11.Ctx{}
+	handle := pkcs11.SessionHandle(1)
+
+	session := NewSession(ctx, handle)
+
+	if session.ctx != ctx {
+		t.Errorf("Expected context to be %v, but got %v", ctx, session.ctx)
+	}
+
+	if session.handle != handle {
+		t.Errorf("Expected handle to be %v, but got %v", handle, session.handle)
+	}
+}
+
+func TestNewPrivateKeyRSA(t *testing.T) {
+	handle := pkcs11.ObjectHandle(2)
+
+	privateKey := NewPrivateKeyRSA(handle)
+
+	if privateKey.handle != handle {
+		t.Errorf("Expected handle to be %v, but got %v", handle, privateKey.handle)
+	}
+}
 
 func TestHashToPKCS11Success(t *testing.T) {
 	testCases := []struct {
@@ -28,5 +54,18 @@ func TestHashToPKCS11Success(t *testing.T) {
 		if hashAlg != tc.expectedHash || mgfAlg != tc.expectedMGF || hashLen != tc.expectedLen || err != tc.expectedError {
 			t.Errorf("For input %v, expected (%v, %v, %v, %v), but got (%v, %v, %v, %v)", tc.inputHash, tc.expectedHash, tc.expectedMGF, tc.expectedLen, tc.expectedError, hashAlg, mgfAlg, hashLen, err)
 		}
+	}
+}
+
+func TestError(t *testing.T) {
+	expectedResult := "hsm decrypt error"
+	output := Error.Error(ErrHsmDecrypt)
+
+	if reflect.TypeOf(output).String() != "string" {
+		t.Error("Expected string")
+	}
+
+	if output != expectedResult {
+		t.Errorf("Output %v not equal to expected %v", output, expectedResult)
 	}
 }
