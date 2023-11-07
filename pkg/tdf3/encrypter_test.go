09d69f6db6f8c33ef17a2bb983f0c796dbdbdb77
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2023-11-07 18:03:37 +0200

    Add TestEncryptWithPublicKeyFailure

---
+++ pkg/tdf3/encrypter_test.go
@@ -0,0 +1,28 @@
+package tdf3
+
+import (
+	"crypto/rsa"
+	"math/big"
+	"testing"
+)
+
+func TestEncryptWithPublicKeyFailure(t *testing.T) {
+	// Small size of PublicKey
+	mockKey := &rsa.PublicKey{
+		N: big.NewInt(123),
+		E: 2048,
+	}
+
+	var i interface{}
+	i = mockKey
+
+	t.Log(mockKey.Size())
+
+	output, err := EncryptWithPublicKey([]byte{}, &i)
+
+	t.Log(output)
+
+	if err == nil {
+		t.Errorf("Expected  error, but got: %v", err)
+	}
+}
