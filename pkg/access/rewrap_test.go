0bf43bb0ca62be3dd9ddf520b8df414b3437d5eb
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2023-12-20 17:06:37 +0200

    Added tests for Handler

--- pkg/access/rewrap_test.go
+++ pkg/access/rewrap_test.go
@@ -39,7 +39,7 @@ func TestHandlerEmptyRequestFailure(t *testing.T) {
 	}
 }
 
-func TestHandlerAuthFailure(t *testing.T) {
+func TestHandlerAuthFailure0(t *testing.T) {
 	body := `{"mock": "value"}`
 	request, _ := http.NewRequest(http.MethodGet, "/v2/rewrap", bytes.NewBufferString(body))
 	request.Header.Set("Content-Type", "application/json")
@@ -63,5 +63,63 @@ func TestHandlerAuthFailure(t *testing.T) {
 
 	if strings.Compare(result, "401 Unauthorized") != 0 {
 		t.Errorf("got %s, but should return error", result)
+	}
+}
+
+func TestHandlerAuthFailure1(t *testing.T) {
+	body := `{"mock": "value"}`
+	request, _ := http.NewRequest(http.MethodGet, "/v2/rewrap", bytes.NewBufferString(body))
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bearer invalidToken")
+
+	response := httptest.NewRecorder()
+
+	kasURI, _ := url.Parse("https://" + hostname + ":5000")
+	kas := Provider{
+		URI:          *kasURI,
+		PrivateKey:   p11.Pkcs11PrivateKeyRSA{},
+		PublicKeyRsa: rsa.PublicKey{},
+		PublicKeyEc:  ecdsa.PublicKey{},
+		Certificate:  x509.Certificate{},
+		Attributes:   nil,
+		Session:      p11.Pkcs11Session{},
+		OIDCVerifier: nil,
+	}
+
+	kas.Handler(response, request)
+
+	resultStatus := response.Result().Status
+
+	if strings.Compare(resultStatus, "400 Bad Request") != 0 {
+		t.Errorf("got %s, but should return error", resultStatus)
+	}
+}
+
+func TestHandlerAuthFailure2(t *testing.T) {
+	body := `{"mock": "value"}`
+	request, _ := http.NewRequest(http.MethodGet, "/v2/rewrap", bytes.NewBufferString(body))
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "invalidToken")
+
+	response := httptest.NewRecorder()
+
+	kasURI, _ := url.Parse("https://" + hostname + ":5000")
+	kas := Provider{
+		URI:          *kasURI,
+		PrivateKey:   p11.Pkcs11PrivateKeyRSA{},
+		PublicKeyRsa: rsa.PublicKey{},
+		PublicKeyEc:  ecdsa.PublicKey{},
+		Certificate:  x509.Certificate{},
+		Attributes:   nil,
+		Session:      p11.Pkcs11Session{},
+		OIDCVerifier: nil,
+	}
+
+	kas.Handler(response, request)
+
+	resultStatus := response.Result().Status
+
+	if strings.Compare(resultStatus, "400 Bad Request") != 0 {
+		t.Errorf("got %s, but should return error", resultStatus)
 	}
 }
