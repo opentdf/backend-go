ae75e9069739c6cb7db394e905c6ad51f93a4430
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2024-02-12 17:17:42 +0200

    Upsert test

--- plugins/revocation_test.go
+++ plugins/revocation_test.go
@@ -50,3 +50,46 @@ func TestUpdateRandomId(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusForbidden)
 	}
 }
+
+func TestUpsertValidEntity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("entity", "mockId")
+	rr := httptest.NewRecorder()
+
+	Upsert(mockHandler)(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestUpsertBlockedEntity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("entity", "blockedId")
+	rr := httptest.NewRecorder()
+
+	Upsert(mockHandler)(rr, req)
+
+	if status := rr.Code; status != http.StatusForbidden {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusForbidden)
+	}
+
+	expectedBody := "Access denied"
+	if body := rr.Body.String(); body != expectedBody {
+		t.Errorf("handler returned unexpected body: got %v want %v", body, expectedBody)
+	}
+}
+
+func TestUpsertRandomId(t *testing.T) {
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("entity", "randomId")
+	rr := httptest.NewRecorder()
+
+	// Call the Update function with a mock handler
+	Upsert(mockHandler)(rr, req)
+
+	// Check the response status code
+	if status := rr.Code; status != http.StatusForbidden {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusForbidden)
+	}
+}
